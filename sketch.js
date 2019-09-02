8166b3bf67012944b6c88b6c2742358054868d0a
aheb157 <aheb157@lsu.edu>
2019-09-02 16:18:56 -0500

    first project finished

--- sketch.js
+++ sketch.js
@@ -1,12 +1,132 @@
 function setup() {
-ellipse (50, 50, 80, 80)
+    createCanvas(1500, 700);
+    background(0)
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        fill (0);
-    } else {
-        fill (255);
-    }
-    //ellipse(50,50,80,80)
+ fill(255);
+ellipse(width/2, height/2, 500, 500);
+fill(0);
+ellipse(width/2, height/2, 300, 300); 
+fill(255);
+ellipse(275, 175, 200, 200);  
+fill(0);
+ellipse(275, 175, 100, 100); 
+fill(255);
+ellipse(1225, 525, 200, 200);
+fill(0);
+ellipse(1225, 525, 100, 100);
+fill(255);
+ellipse(275, 525, 200, 200);
+fill(0);
+ellipse(275, 525, 100, 100);
+fill(255);
+ellipse(1225, 175, 200, 200);
+fill(0);
+ellipse(1225, 175, 100, 100);
+fill(0);
+ellipse(980, height/2, 250, 250);
+fill(255);
+ellipse(980, height/2, 200, 200);
+fill(0);
+ellipse(530, height/2, 250, 250);
+fill(255);
+ellipse(530, height/2, 200, 200);
+
+fill(0);
+ellipse(190, 175, 100, 100);
+fill(255);
+ellipse(190, 175, 50, 50);
+fill(0);
+ellipse(355, 175, 100, 100);
+fill(255);
+ellipse(355, 175, 50, 50);
+fill(0);
+ellipse(275, 100, 100, 100);
+fill(255);
+ellipse(275, 100, 50, 50);
+fill(0);
+ellipse(275, 255, 100, 100);
+fill(255);
+ellipse(275, 255, 50, 50);
+
+fill(0);
+ellipse(1225, 100, 100, 100);
+fill(255);
+ellipse(1225, 100, 50, 50);
+fill(0);
+ellipse(1225, 255, 100, 100);
+fill(255);
+ellipse(1225, 255, 50, 50);
+fill(0);
+ellipse(1300, 175, 100, 100);
+fill(255);
+ellipse(1300, 175, 50, 50);
+fill(0);
+ellipse(1150, 175, 100, 100);
+fill(255);
+ellipse(1150, 175, 50, 50);
+
+fill(0);
+ellipse(1225, 450, 100, 100);
+fill(255);
+ellipse(1225, 450, 50, 50);
+fill(0);
+ellipse(1225, 600, 100, 100);
+fill(255);
+ellipse(1225, 600, 50, 50);
+fill(0);
+ellipse(1300, 525, 100, 100);
+fill(255);
+ellipse(1300, 525, 50, 50);
+fill(0);
+ellipse(1150, 525, 100, 100);
+fill(255);
+ellipse(1150, 525, 50, 50);
+
+fill(0);
+ellipse(275, 450, 100, 100);
+fill(255);
+ellipse(275, 450, 50, 50);
+fill(0);
+ellipse(275, 600, 100, 100);
+fill(255);
+ellipse(275, 600, 50, 50);
+fill(0);
+ellipse(190, 525, 100, 100);
+fill(255);
+ellipse(190, 525, 50, 50);
+fill(0);
+ellipse(355, 525, 100, 100);
+fill(255);
+ellipse(355, 525, 50, 50);
+
+fill(0);
+ellipse(width/2, 155, 250, 250);
+fill(255);
+ellipse(width/2, 155, 200, 200);
+fill(0);
+ellipse(width/2, 550, 250, 250);
+fill(255);
+ellipse(width/2, 550, 200, 200);
+
+fill(0);
+ellipse(980, height/2, 150, 150);
+fill(255);
+ellipse(980, height/2, 75, 75);
+fill(0);
+ellipse(530, height/2, 150, 150);
+fill(255);
+ellipse(530, height/2, 75, 75);
+fill(0);
+ellipse(width/2, 155, 150, 150);
+fill(255);
+ellipse(width/2, 155, 75, 75);
+fill(0);
+ellipse(width/2, 550, 150, 150);
+fill(255);
+ellipse(width/2, 550, 75, 75);
+
+fill(255);
+ellipse(width/2, height/2, 100, 100);
 }
